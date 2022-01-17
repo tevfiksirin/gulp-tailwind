9b316ceaef31c045b79afa83f507936e29820621
tevfiksirin <tevfiksirin@gmail.com>
2022-01-17 11:44:50 +0300

    config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,5 @@ module.exports = {
   },
   variants: {},
   plugins: [],
-  presets: [
-    require('tailwindcss/defaultConfig'), require('xtendui/tailwind.preset'),
-  ],
-  // put other purge content e.g.: './src/**/*.{html,js}'
-  content: ['./node_modules/xtendui/src/*[!.css].js'],
+
 };
